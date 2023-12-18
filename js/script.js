fbd6d74e48259b4c273c8b4a62af876dbbe49005
Axhu99 <xhuliodreni@icloud.com>
2023-12-18 15:23:20 +0100

    lineup

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* 
+    1- chiedere all'utente quanti chilometri dovra percorre
+    2- chiedere all'utente quanti anni ha
+    3- calcolare in base ai chilometri indicati quanto dovra pagare
+    4- se ha diritto a uno sconto 
+    4b- se si a quale sconto
+    5- avere un risultato in forma umana (due cifre dopo la virgola)
+    6(bonus)- stampare il risultato 
+*/
