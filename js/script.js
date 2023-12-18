4b728cea7537201c37b5deb73b3c270eb4d5e097
Axhu99 <xhuliodreni@icloud.com>
2023-12-18 16:39:38 +0100

    added check

--- js/script.js
+++ js/script.js
@@ -9,18 +9,31 @@
 */
 
 // 1- chiedere all'utente quanti chilometri dovra percorre
-
-const kmToDo = prompt('Quanti CHILOMETRI deve fare?', '100');
-console.log(kmToDo);
-
 // 2- chiedere all'utente quanti anni ha
+const element = document.getElementById('howmuch');
 
-const utenteAge = prompt('Quanti ANNI ha?', '17');
-console.log(utenteAge);
+const priceForKm = 0.21;
+let price = 0;
+
+const kmToDo = parseInt(prompt('Quanti CHILOMETRI deve fare?', '100'));
+const utenteAge = parseInt(prompt('Quanti ANNI ha (scrivere solo il numero)?', '17'));
+console.log("chilometri da fare:", kmToDo, "eta utente", utenteAge);
+
+let errorMessage
+let discountMessage = 'Non hai diritto a nessuno sconto'
+const arrow = '-->'
+
+if(isNaN(kmToDo) || isNaN(utenteAge) || kmToDo < 0 || utenteAge < 0){
+    errorMessage = 'Spiacente hai sbagliato qualcosa'
+}
+
+if (errorMessage){
+    alert(errorMessage);
+} else {
 
 // 3- calcolare in base ai chilometri indicati quanto dovra pagare
 
-let price = kmToDo * 0.21
+let price = kmToDo * priceForKm
 console.log(price)
 
 // 4- se ha diritto a uno sconto 
@@ -28,9 +41,11 @@ console.log(price)
 
 if(utenteAge < 18 ){
     price *= 0.8
+    discountMessage = 'Hai diritto allo SCONTO del 20%'
     console.log(price)
 } else if(utenteAge > 65){
     price *= 0.6
+    discountMessage = 'Hai diritto allo SCONTO del 40%'
     console.log(price)
 }
 
@@ -40,7 +55,6 @@ const realPrice = price.toFixed(2)
 console.log(realPrice) 
 
 // 6(bonus)- stampare il risultato
-
-const element = document.getElementById('howmuch');
-const message = `Il suo viaggio costa ${realPrice} euro`
+let message = `${discountMessage} ${arrow} Il suo viaggio costa ${realPrice} euro`
 element.innerText = message;
+}
